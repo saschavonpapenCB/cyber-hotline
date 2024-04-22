48bd599a4c85a046cc4e7ebed672723fe0e42c39
saschavonpapenCB <sascha.vonpapen@couchbase.com>
2024-04-22 19:48:55 +1000

    Created routes.

--- cyber-website/src/app/app.module.ts
+++ cyber-website/src/app/app.module.ts
@@ -4,11 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ToolsComponent } from './tools/tools.component';
+import { HealthcheckComponent } from './healthcheck/healthcheck.component';
+import { PartnersComponent } from './partners/partners.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HelplineComponent } from './helpline/helpline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    AboutComponent,
+    ToolsComponent,
+    HealthcheckComponent,
+    PartnersComponent,
+    LoginComponent,
+    SignupComponent,
+    HelplineComponent
   ],
   imports: [
     BrowserModule,
